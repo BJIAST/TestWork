4449414e7ac6bd41d7b04099aa8857a3cc2f3dd9
BJIAST <bjiast.01@gmail.com>
2016-11-15 20:13:02 +0200

    done ip upload

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php 
-$mysqli = new mysqli("localhost", "u519406778_root", "123456", "u519406778_tdb");
-echo $mysqli->connect_error;
- ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- source/connect.php
+++ source/connect.php
@@ -1,6 +1,31 @@
-<?php 
+﻿<?php 
 
-$mysqli = new mysqli("localhost", "u519406778_root", "123456", "u519406778_tdb");
-echo $mysqli->connect_error;
+function connect_db(){
+    $mysqli = new mysqli("localhost", "root", "", "testdb");
+    
+    if( $mysqli->connect_error){
+        die('Connect Error: ' . $mysqli->connect_error);
+    }
+    return  $mysqli;
+}
+function select_list(){
+    $mysqli = connect_db();
+    $userIp = $_SERVER["REMOTE_ADDR"];
 
+    $sql = "SELECT * from squarepos WHERE ipadress = '$userIp'";
+    if ($result = $mysqli->query($sql)){
+        if ($result->num_rows > 0){
+           echo "Стою!";
+       }else{
+            $top=0;
+            $left=0;
+           $sqlIns = "INSERT INTO squarepos VALUES(NULL,'$userIp','$top','$left')";
+           $mysqli->query($sqlIns);
+       }
+   }else{
+    echo 'Неудачно! ' . $mysqli->error;
+}
+}
+print_r(select_list());
+echo "<br>";
 ?>
